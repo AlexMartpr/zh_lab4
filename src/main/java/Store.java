19644731d72717eff3271c7d5a683a96cddc2a5c
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:48:32 +0300

    Scripted auto-commit on change (2021-12-04 02:48:32) by gitwatch.sh

---
+++ src/main/java/Store.java
@@ -0,0 +1,13 @@
+import akka.actor.AbstractActor;
+
+public class Store extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+    
+}
