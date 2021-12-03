b03f501ed6570230990a7c48f2cd04eb65cb7ffb
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:26:57 +0300

    Scripted auto-commit on change (2021-12-04 02:26:57) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Test implements Serializable {
     private String testName;
     private String expectedResult;
-    private Test parentTest;
+    private TestGet parentTest;
     private Object[] params;
 
     private static final String TEST_NAME = "testName";
@@ -36,7 +36,7 @@ public class Test implements Serializable {
         return this.params;
     }
 
-    public void setParentTest(Test p) {
+    public void setParentTest(TestGet p) {
         this.parentTest = p;
     }
 
