f65324efbc6c4fe5802d5900da36b413b0c2499d
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:56:39 +0300

    Scripted auto-commit on change (2021-12-04 02:56:39) by gitwatch.sh

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class Store extends AbstractActor {
     private Map<String, ArrayList<Test>> store = new HashMap<>();
@@ -11,9 +12,26 @@ public class Store extends AbstractActor {
     private static final String FAIL = "FAIL";
     private static final String SUCCESS = "SUCCESS";
 
+    private void putTest(Test test) {
+        String pId = test.getParentTest().getPackageId();
+        if (this.store.containsKey(pId)) {
+            this.store.get(pId).add(test);
+        } else {
+            ArrayList<Test> arrayTests = new ArrayList<>();
+            arrayTests.add(test);
+            this.store.put(pId, arrayTests);
+        }
+    }
+
+    private Map<String, String> prepareRes
+
     @Override
     public Receive createReceive() {
-        
+        return ReceiveBuilder
+                .create()
+                .match(Test.class, t -> putTest(t))
+                .match(String.class, id -> sender().tell(prepareRes(id), self()))
+                .build();
     }
 
     
