ea230c48696c7e87d2c7237cb332fd7158625b14
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:52:39 +0300

    Scripted auto-commit on change (2021-12-04 02:52:39) by gitwatch.sh

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,11 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import akka.actor.AbstractActor;
 
 public class Store extends AbstractActor {
+    private Map<String, ArrayList<Test>> store = new HashMap<>();
+
+    private static final String FAIL = "FAIL";
+    private static final String SUCCESS = "SUCCESS";
 
     @Override
     public Receive createReceive() {
-        // TODO Auto-generated method stub
-        return null;
+        
     }
 
     
