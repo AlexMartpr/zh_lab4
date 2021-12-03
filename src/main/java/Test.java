d5117bf71b1e434d2800ebf29305b04755a87d00
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:09:47 +0300

    Scripted auto-commit on change (2021-12-04 02:09:47) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,38 @@
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test implements Serializable {
     private String testName;
     private String expectedResult;
     private Object[] params;
 
-    private static final TEST_NAME = "testName";
-    private static final
+    private static final String TEST_NAME = "testName";
+    private static final String EXPECTED_RESULT = "expectedResult";
+    private static final String PARAMS = "params";
+    
+    @JsonCreator
+    public Test(
+            @JsonProperty(TEST_NAME) String tn,
+            @JsonProperty(EXPECTED_RESULT) String er,
+            @JsonProperty(PARAMS) Object[] p
+    ) {
+        this.testName = tn;
+        this.expectedResult = er;
+        this.params = p;
+    }
+
+    public String getTestName() {
+        return this.testName;
+    }
+
+    public String getExpectedResult() {
+        return this.expectedResult;
+    }
+
+    public Object[] getParams() {
+        return this.params;
+    }
+
 }
