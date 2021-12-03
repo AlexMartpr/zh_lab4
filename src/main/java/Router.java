b3cf2355e074d57009bfaa4a370787c813349024
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:11:16 +0300

    Scripted auto-commit on change (2021-12-04 02:11:16) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,3 +1,11 @@
-public class Router {
+import akka.actor.AbstractActor;
+
+public class Router extends AbstractActor {
+
+    @Override
+    public Receive createReceive() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
