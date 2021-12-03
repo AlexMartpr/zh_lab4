19ff641af316897a24764198d23395c67f616f33
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 01:42:06 +0300

    Scripted auto-commit on change (2021-12-04 01:42:06) by gitwatch.sh

--- src/main/java/RouterDefine.java
+++ src/main/java/RouterDefine.java
@@ -1,12 +1,28 @@
+import java.util.regex.Pattern;
+
 import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
 
 public class RouterDefine extends AllDirectives {
     private final ActorRef router;
-    
+    private final String PARAMETR_NAME = "packageId";
+    private final int TIME_OUT = 3000;
+
     public RouterDefine(ActorRef r) {
         this.router = r;
     }
 
-    public Route createRoute
+    public Route createRoute() {
+        return concat(
+            get(() -> 
+                parameter(PARAMETR_NAME, id -> {
+                    Future<Object> res = Patterns.ask(router, id,TIME_OUT);
+                    return completeOKWithFuture(res, Jackson.marshaller());
+            })),
+            post(() ->
+                entity() )
+        )
+    }
 }
