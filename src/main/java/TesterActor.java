367fb29088a8e9a68765f5be8c6688947404afe6
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:46:09 +0300

    Scripted auto-commit on change (2021-12-04 02:46:09) by gitwatch.sh

--- src/main/java/TesterActor.java
+++ src/main/java/TesterActor.java
@@ -1,7 +1,12 @@
 import java.io.Serializable;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
 import akka.http.scaladsl.model.headers.CacheDirectives.private;
 
 public class TesterActor extends AbstractActor{
@@ -9,12 +14,34 @@ public class TesterActor extends AbstractActor{
     
     private static final String JS_VER = "nashorn";
 
+    public TesterActor(ActorRef ac) {
+        this.storeActor = ac;
+    }
 
+    private String runTest(Test test) {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_VER);
+        TestMessage parent = test.getParentTest();
+        engine.eval(parent.getTypeScript());
+        Invocable invoc = (Invocable) engine;
+        return invoc.invokeFunction(parent.getNameFunction(), test.getParams()).toString();
+    }
+
+    private Test checkResult(Test test) {
+        try {
+            String getRes = runTest(test);
+            test.setActualResult(getRes);
+        } catch (Exception excep) {
+            test.setActualResult(excep.toString());
+        }
+        return test;
+    }
 
     @Override
     public Receive createReceive() {
-        // TODO Auto-generated method stub
-        return null;
+        return ReceiveBuilder
+                .create()
+                .match(Test.class, msg -> storeActor.tell(checkResult(msg), ActorRef.noSender()))
+                .build();
     }
 
     
