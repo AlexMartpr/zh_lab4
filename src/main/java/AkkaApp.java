9aeb1fc4ab56e73f18d22309051ce8bfcf3ea1ab
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 01:27:27 +0300

    Scripted auto-commit on change (2021-12-04 01:27:27) by gitwatch.sh

--- src/main/java/AkkaApp.java
+++ src/main/java/AkkaApp.java
@@ -1,8 +1,12 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class AkkaApp {
     public static void main(String[] args) {
@@ -12,6 +16,6 @@ public class AkkaApp {
         final Http http = Http.get(sys);
         final ActorMaterializer actorMat = ActorMaterializer.create(sys);
         final AkkaApp instance = new AkkaApp();
-         
+        final Flow<HttpRequest, HttpResponse, NotUsed> flow =  
     }
 }
