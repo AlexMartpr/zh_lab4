85e02045f897c723ee07e4a4607666c5e9818b7c
AlexMartPr <alexmartpr@gmail.com>
2021-12-04 02:04:49 +0300

    Scripted auto-commit on change (2021-12-04 02:04:49) by gitwatch.sh

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Test implements Serializable {
+    private String testName;
+    private String expectedResult;
+    private Object[] params;
+
+    
+}
